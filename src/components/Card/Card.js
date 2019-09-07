7f90868846e3ebb3f1cfa2dcda3cf9d652aba1f1
Stvn_Jvr <estevenjaviier@gmail.com>
2019-09-07 16:02:19 -0500

    Bb

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,7 @@ function Card() {
   return (
     <div className="container">
         <div className="row">
-            <div className="col-md-4 haciaDerecha">X
+            <div className=" haciaDerecha">X
                 <div className="card mb-4 shadow-sm">
                     <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
                     <div className="card-body">
