06d5abedb66cb08e7b92331c6e813e48ec3e40c6
Stvn_Jvr <estevenjaviier>
2019-09-10 09:47:33 -0500

    agregue imagen

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 function Card() {
+    const img = 'https://scontent-bog1-1.xx.fbcdn.net/v/t1.0-9/42600689_1257222957753118_8355322505967173632_n.jpg?_nc_cat=100&_nc_oc=AQmjkQWvRxeFnpDCRqA4VD075AKwjebcN0YHzFpOyMyYnst7hC-HKo7HFdGZyeFggfM&_nc_ht=scontent-bog1-1.xx&oh=111cc1d95efd6c6c3a8adc9ab21a000a&oe=5DF414E8';
   return (
     <div className="container">
         <div className="row">
             <div className="col-md-4">
                 <div className="card mb-4 shadow-sm">
-                    <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <img className="bd-placeholder-img card-img-top" width="100%" height="225" href="{img}" />
                     <div className="card-body">
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <div className="d-flex justify-content-between align-items-center">
