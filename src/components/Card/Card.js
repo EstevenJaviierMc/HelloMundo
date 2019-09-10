cdd6cbde1d8c57b2b51e2cb81a622245ce94b3ca
Stvn_Jvr <estevenjaviier>
2019-09-10 09:49:44 -0500

    quite comillas

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,7 +7,7 @@ function Card() {
         <div className="row">
             <div className="col-md-4">
                 <div className="card mb-4 shadow-sm">
-                    <img className="bd-placeholder-img card-img-top" width="100%" height="225" href="{img}" />
+                    <img className="bd-placeholder-img card-img-top" width="100%" height="225" href={img} />
                     <div className="card-body">
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <div className="d-flex justify-content-between align-items-center">
