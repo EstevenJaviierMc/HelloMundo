9c79364685c604cce9ced4caac725771510f81c5
Stvn_Jvr <estevenjaviier>
2019-09-10 09:41:53 -0500

    Cambie el nombre

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
         <Nabvar/>
         <section class="jumbotron text-center">
             <div class="container">
-            <h1 class="jumbotron-heading">Belleza Tropical!!!!!</h1>
+            <h1 class="jumbotron-heading">Glenis Ramos porno</h1>
             <p class="lead text-muted">In this page you can public the Posts about the topic whatever, only in the cours that you be suscrito!</p>
             <p>
                 <a href="#" class="btn btn-primary my-2">Main call to action</a>
@@ -19,13 +19,7 @@ function Home() {
         </section>
         <div className="album py-5 bg-light">
             <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+
         </div>
       </div>
   );
