62bbc61ebf2e48ea289f89a974d41fa4829e9e1b
Stvn_Jvr <estevenjaviier@gmail.com>
2019-09-12 13:28:27 -0500

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 function Card() {
     const img = 'https://scontent-bog1-1.xx.fbcdn.net/v/t1.0-9/42600689_1257222957753118_8355322505967173632_n.jpg?_nc_cat=100&_nc_oc=AQmjkQWvRxeFnpDCRqA4VD075AKwjebcN0YHzFpOyMyYnst7hC-HKo7HFdGZyeFggfM&_nc_ht=scontent-bog1-1.xx&oh=111cc1d95efd6c6c3a8adc9ab21a000a&oe=5DF414E8';
-  return (
+  constructor(props){
+super(props);
+this.state = { fotos:[] };
+}
+componentWillMount(){
+  fetch('http://taller-angular.carlosazaustre.es/empleados')
+      .then((response) => {
+        return response.json()
+      })
+      .then((fotos) => {
+        this.setState({ fotos: fotos })
+      })
+
+}
+
+return (
     <div className="container">
         <div className="row">
             <div className="col-md-4">
